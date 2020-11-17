c1e4f48a43fd233c5ef10b4c662af37bf27d3baf
Viet Pham <vietpham@viets-mbp.home>
2020-11-17 20:59:02 +1100

    finished

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -4,6 +4,10 @@ import ResultsDetail from '../components/ResultsDetail';
 import { withNavigation } from 'react-navigation';
 
 const ResultsList = ({ title, results, navigation }) => {
+  if (!results.length) {
+    return null;
+  }
+  
   return (
     <View style={styles.container}>
       <Text style={styles.titleStyle}>{title}</Text>
@@ -15,7 +19,7 @@ const ResultsList = ({ title, results, navigation }) => {
         renderItem={({ item }) => {
           return (
             <TouchableOpacity 
-              onPress={() => navigation.navigate("ResultsShow")}
+              onPress={() => navigation.navigate("ResultsShow", { id: item.id })}
             >
               <ResultsDetail result={item} />
             </TouchableOpacity>
